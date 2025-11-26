4d178e6ce9f369add8254cf0433cf017d8954ac8
Muhammad Ahmed <muhammadahmedraza.development@gmail.com>
2025-11-26 13:21:32 +0500

    Firebase:Connecting javascript with firebase

---
+++ firebase.js
@@ -0,0 +1,18 @@
+
+ // Import the functions you need from the SDKs you need
+  import { initializeApp  } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyARugKmr5qHd0bTydfzsFs6oWN694uKvBM",
+    authDomain: "fir-overview-ff349.firebaseapp.com",
+    projectId: "fir-overview-ff349",
+    storageBucket: "fir-overview-ff349.firebasestorage.app",
+    messagingSenderId: "795666167238",
+    appId: "1:795666167238:web:09550a6c07c34b89a72ec7"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  
+// Initialize Firebase Authentication and get a reference to the service
